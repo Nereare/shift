6a433b9602958090371264f63995f4684d837d6c
Igor Padoim <igorpadoim@gmail.com>
2025-07-16 23:29:38 -0300

    Begin ruby script

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+ruby file: ".ruby-version"
+
+gem 'clipboard'
+gem 'ffi', :platforms => [:mswin, :mingw]
---
+++ main.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+require 'clipboard'
+
+Clipboard.copy('"Doutores aguardo a devolutiva à respeito dessa escala, por favor confirmem ou não o plantão
+
+#Escala de *Quinta-feira 17/07/2025*
+#HSPE - SERVIDOR ESTADUAL 
+#Diurno (07-19)
+
+
+*CHEFIA DE PLANTÃO* 
+Dr (a)        IGOR PADOIM                        
+
+*EQUIPE MÉDICA* 
+🩺 _*CLÍNICA: 07-19h*_
+Dr (a)        Ana Clara Souza 
+Dr (a)        Pedro Henrique Santos 
+Dr (a)        Maria Eduarda Oliveira 
+Dr (a)        João Gabriel Costa 
+Dr (a)        Isabela Lima 
+Dr (a)        Arthur Pereira 
+
+🩺  _*CLÍNICA: 10-16h*_
+Dr (a)        Laura Martins
+Dr (a)         Lucas Rocha 
+
+🩺  _*CLÍNICA: 13-19h*_
+Dr (a)        Manuela Fernandes
+
+🩺>🏥 _*OBS. TÉRREO*_
+Dr (a)        Enzo Gabriel Almeida
+Dr (a)        Valentina Ribeiro 07:00 às 13:00   - Samuel Gomes 13:00 às 19:00 
+
+>🏥 _*ENFERMARIA  A*_
+Dr (a)        Alice Carvalho
+
+>🏥 _*ENFERMARIA B e C*_
+Dr (a)        Miguel Barbosa
+Dr (a)        Sophia Xavier 
+
+>🏥 _*UCP*_
+Dr (a)        Davi Mendes 
+
+🚨 _*EMERGÊNCIA*_
+Dr (a)        Helena Dias 
+
+⚡ _*DOR TORÁCICA*_
+Dr (a)        Bernardo Nunes  07:00 às 13:00 Lívia Silveira 13:00 às 19:00
+
+🧬 _*CIRURGIA:*_
+Dr (a)        Rafael Correia
+Dr (a)        Heitor Ferreira
+
+🩻 _*ORTOPEDIA*_
+Dr (a)        Gabriela Castro    07:00 às 13:00  / Dr (a) Nicolas Santos13:00 às 19:00
+Dr (a)        Julia Rocha  07:00 às 13:00  / Dr (a) Felipe Nascimento 13:00 às 19:00 
+
+
+🧸 _*PEDIATRIA*_ 
+Dr (a)        Cauã Rodrigues
+
+> Escala sujeita à alterações <"')
+
+foo = Clipboard.paste
+puts foo
